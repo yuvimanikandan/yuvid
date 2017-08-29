dc85c5307d4a88c016146d919853eb3369258539
yuvimanikandan <31242255+yuvimanikandan@users.noreply.github.com>
2017-08-29 16:03:55 +0530

    Update even.c

--- even.c
+++ even.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+ 
 int main()
 {
-    int number;
-
-    printf("Enter an integer: ");
-    scanf("%d", &number);
-
-    // True if the number is perfectly divisible by 2
-    if(number % 2 == 0)
-        printf("%d is even.", number);
-    else
-        printf("%d is odd.", number);
-
-    return 0;
+   int n;
+ 
+   printf("Enter an integer\n");
+   scanf("%d", &n);
+ 
+   if (n%2 == 0)
+      printf("Even\n");
+   else
+      printf("Odd\n");
+ 
+   return 0;
 }
